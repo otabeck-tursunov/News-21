6c16d69d832f35e2024f59693abbd3d8e50da161
unknown <otabecktursunov@gmail.com>
2024-12-05 17:09:50 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
